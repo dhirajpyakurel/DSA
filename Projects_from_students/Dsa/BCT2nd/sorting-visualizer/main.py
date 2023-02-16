7fecfa783f32927b4c645707f611a002f25f90be
uppercasee <70530668+uppercasee@users.noreply.github.com>
2023-02-16 19:41:47 +0545

    Initial Commit

---
+++ Projects_from_students/Dsa/BCT2nd/sorting-visualizer/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Project: SortingVisualizer
+# Date: 2023/02/15
+# Version: 1.0.5
+# Description: This program is a visualizer for sorting algorithms. It uses tkinter to create a GUI that displays the sorting process.
+# Class: BCT 'A'
+# Authors: 1. Aashish Pokharel (01)
+#          2. Aashutosh Joshi (02)
+#          3. Ayush Acharya (21)
+
+from sorting_visualizer import SortVisualizer
+
+if __name__ == "__main__":
+    SortVisualizer()
